4cfd9594283730ae104fd1dfcc24585cf6b5af33
simon <gatheru.simon432@gmail.com>
2019-04-17 13:06:51 +0300

    call get movies function in views to be rendered, and loop movie attributes on index.html

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from flask import render_template
 from app import app
+from . import get_movies
 
 # Views
 @app.route('/')
 def index():
-
+    popular_movies = get_movies('popular')
     title = "Home - Welcome to the best movie review website"
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=title, popular=popular_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):

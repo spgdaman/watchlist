4beb944d510a04fe8dc0023de1fd9282d4b89aa4
simon <gatheru.simon432@gmail.com>
2019-04-17 13:02:07 +0300

    define get movie function that consumes api

--- app/requests.py
+++ app/requests.py
@@ -4,4 +4,12 @@ import requests
 # Getting api key
 api_key = app.config['MOVIE_API_KEY']
 
-def get_movie():
+def get_movie(category):
+    api_url = 'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(category,api_key)
+
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+

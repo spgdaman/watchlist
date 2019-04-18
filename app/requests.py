2f6fe6b170c7b2a767a72fa09d8e2834df367833
simon <gatheru.simon432@gmail.com>
2019-04-18 16:19:27 +0300

    search functionality add and movie constructor implemented to fix pictures not displaying

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,9 @@
 from app import app
+from .models import movie
 import requests
+import urllib.request, json
+
+Movie = movie.Movie
 
 # Getting api key
 api_key = app.config['MOVIE_API_KEY']
@@ -9,24 +13,88 @@ def get_movie(category):
 
     response = requests.get(api_url)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["results"]
+    data = response.json()
+    dict_list = data["results"]    
+    
+    results = process_results(dict_list)    
+
+    return results
+
+    # with urllib.request.urlopen(api_url) as url:
+    #     get_movies_data = url.read()
+    #     get_movies_response = json.loads(get_movies_data)
+
+    #     movie_results = None
+
+    #     if get_movies_response['results']:
+    #         movie_results_list = get_movies_response['results']
+    #         movie_results = process_results(movie_results_list)
+
+    # return movie_results
 
 def show_movie(id):
     api_url = 'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(id,api_key)
 
     response = requests.get(api_url)
+   
+    data = response.json()
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
+    if data:
+        id = data.get('id')
+        title = data.get('original_title')
+        overview = data.get('overview')
+        poster = data.get('poster_path')
+        vote_average = data.get('vote_average')
+        vote_count = data.get('vote_count')
+
+    results = Movie(id,title,overview,poster,vote_average,vote_count)
+    return results
+    
+
 
 def search_movie(movie_name):
-    api_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(apikey,movie_name)
+#     api_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
 
-    response = requests.get(api_url)
+#     response = requests.get(api_url)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
+#     data = response.json()
+#     dict_list = data["results"]    
+    
+#     results = process_results(dict_list)    
+
+#     return results
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+    return search_movie_results
+
+def process_results(movie_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    movie_results = []
+    for movie_item in movie_list:
+        id = movie_item['id']
+        title = movie_item.get('original_title')
+        overview = movie_item.get('overview')
+        poster = movie_item.get('poster_path')
+        vote_average = movie_item.get('vote_average')
+        vote_count = movie_item.get('vote_count')
+
+        movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+        movie_results.append(movie_object)
+
+    return movie_results
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, request,redirect,url_for
 from app import app
-from .requests import get_movie, show_movie
+from .requests import get_movie, show_movie, search_movie
 
 # Views
 @app.route('/')
@@ -9,10 +9,24 @@ def index():
     upcoming_movies = get_movie('upcoming')
     now_showing_movies = get_movie('now_playing')
     title = "Home - Welcome to the best movie review website"
-    return render_template('index.html',title=title, popular=popular_movies, upcoming=upcoming_movies, now=now_showing_movies)
+
+    search = request.args.get("movie_query")
+
+    if search:
+        return redirect(url_for('search',movie_name = search))
+    else:
+        return render_template('index.html',title=title, popular=popular_movies, upcoming=upcoming_movies, now=now_showing_movies)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     movie = show_movie(movie_id)
-    title = movie.get("original_title")
+    title = f'{movie.title}'
     return render_template('movie.html', title=title, movie=movie)
+
+@app.route('/search/<string:movie_name>')
+def search(movie_name):
+    right_name = movie_name.split(" ")
+    movie_name_format = "+".join(right_name)
+    searched = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html', search = searched, title=title)
